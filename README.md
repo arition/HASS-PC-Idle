b90de77f182bb52119656647f703c30f30fa0bcc
arition <aritionkb@gmail.com>
2023-01-02 16:06:27 -0800

    feat: Add support for start and close triggers

--- README.md
+++ README.md
@@ -7,19 +7,19 @@ RSSI.
 ## Changes from the original
 
 - Remove webcam based face detection
-- Use `poetry` for dependencies management
+- Use `pdm` for dependencies management
 
 ## Installation
 
 Currently only Windows is supported, ~~Linux support will come some day~~.
 
 1. If you don't have it yet, download Python 3.10 from python.org and install it.
-2. Install `poetry` from <https://python-poetry.org/>
-3. Install dependencies with `poetry install`
+2. Install `pdm` from <https://pdm.fming.dev/latest/>
+3. Install dependencies with `pdm install`
 4. Open config.yaml and enter your HomeAssistant API URL. Could be an IP or a domain name.
 5. Go to your HomeAssistant -> Profile -> and generate a long lived token
 6. Paste it into config after `token:`... Well, if you use HomeAssistant you should know YAML :D
 7. Adjust other parameters to your liking
-8. In the shell type `poetry run python pc_idle.py` and hit enter.
+8. In the shell type `pdm run python pc_idle.py` and hit enter.
 
-You can also use Windows Scheduler to schedule it to start on boot or login.
+You can also use Windows Scheduler to schedule it to start on boot or login. (Use pythonw.exe to hide the window)
--- pc_idle.py
+++ pc_idle.py
@@ -1,4 +1,5 @@
 import asyncio
+import signal
 
 import aiohttp
 import win32api
@@ -12,8 +13,8 @@ def getIdleTime():
     return (win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0
 
 
-async def trigger(action):
-    print(action)
+async def trigger_idle(action):
+    print(f'idle: {action}')
 
     headers = {
         'Authorization': 'Bearer {}'.format(config['token']),
@@ -32,13 +33,45 @@ async def trigger(action):
                 print(e)
 
 
+async def trigger_start(action):
+    print(f'start: {action}')
+
+    headers = {
+        'Authorization': 'Bearer {}'.format(config['token']),
+        'content-type': 'application/json',
+    }
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+        for entity_id in config['home_assistant_start_booleans']:
+            try:
+                url = f"{config['endpoint_url']}/services/input_boolean/{action}"
+                data = {"entity_id": "input_boolean.{}".format(entity_id)}
+                response = await session.post(url, json=data)
+                await response.text()
+                response.close()
+            except Exception as e:
+                print(e)
+
+
 async def main():
-    while True:
+    await trigger_start("turn_on")
+
+    run_loop = True
+
+    async def on_close(*args):
+        nonlocal run_loop
+        run_loop = False
+        await trigger_start("turn_off")
+        return True
+
+    signal.signal(signal.SIGINT, lambda *args: asyncio.create_task(on_close()))
+    signal.signal(signal.SIGTERM, lambda *args: asyncio.create_task(on_close()))
+
+    while run_loop:
         if getIdleTime() > config['idle_time']:
-            await trigger("turn_on")
+            await trigger_idle("turn_on")
         else:
-            await trigger("turn_off")
+            await trigger_idle("turn_off")
         await asyncio.sleep(config['poll_time'])
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+asyncio.run(main())
